5516853883d6f28980fce6013be9de0f5eb1be85
Dennis Dachkovski <dennis@dachkovski.de>
2023-10-16 13:07:34 +0200

    aider: Changed internal server error message to return the actual error message.
    # Aider chat conversation:
    
    USER: can you output what is wrong when error 500?
    
    ASSISTANT: Sure, we can modify the `internal_server_error` function in `app.py` to return the error message when a 500 error occurs. This will help in debugging as the error message will provide more context about what went wrong.
    
    Here is the change:
    
    ```python
    app.py
    <<<<<<< HEAD
    @app.errorhandler(500)
    def internal_server_error(error):
        return jsonify(error="Internal Server Error"), 500
    =======
    @app.errorhandler(500)
    def internal_server_error(error):
        return jsonify(error=str(error)), 500
    >>>>>>> updated
    ```
    
    This change will return the actual error message instead of the generic "Internal Server Error" message.

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def not_found(error):
 
 @app.errorhandler(500)
 def internal_server_error(error):
-    return jsonify(error="Internal Server Error"), 500
+    return jsonify(error=str(error)), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT)
